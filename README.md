dac55cffc9b903c0b6101ec2fe2fc7ac479fd224
sonalidoke11 <sonalidoke810@gmail.com>
2024-08-26 09:36:29 -0400

    Added popup functionality and iterations

---
+++ README.md
@@ -0,0 +1,32 @@
+SBA 316: The Document Object Model
+
+Techical Specification: 
+
+ -  Cache at least one element using selectElementById.
+
+ - Cache at least one element using querySelector or querySelectorAll. 
+
+ - Iterate over a collection of elements to accomplish some task. 10%
+
+ -  Create at least one element using createElement.
+
+ -  Use appendChild and/or prepend to add new elements to the DOM. 
+
+ -  Modify the style and/or CSS classes of an element in response to user interactions
+using the style or classList properties.
+
+ -  Register at least two different event listeners and create the associated event
+handler functions.
+
+ -  Include at least one form and/or input with HTML attribute validation.
+
+ -  Include at least one form and/or input with DOM event-based validation. (This can
+be the same form or input as the one above, but should include event-based
+validation in addition to the HTML attribute validation.)
+
+Functionality added at:
+1) Click on change theme color on banner it will change colors
+2) Click on gifts image under catgories it will show popup with cotent
+3) h2 tags below images in categories section are added through javascript
+4) iteration is added to show image which is having popup inside, hover over images under categories
+5) Added hatml and js validation to signup and login forms
--- js/dom-bom.js
+++ js/dom-bom.js
@@ -1,3 +1,4 @@
+//*** Below part adds H2 tags below Categories images i.e Gifts, Bakery, Food */
 
 // cache element using getElementById
 const cat_gift= document.getElementById("category_gift")
@@ -12,31 +13,27 @@ cat_gift.appendChild(cat_gift_h2);
 
 
 
-// cache element using getElementById
+// for bakery 
 const cat_bakery= document.getElementById("category_Bakery")
 
-// creating h2 tag to display below bakery category
 let cat_bakery_h2 = document.createElement("h2")    
 cat_bakery_h2.innerText ="Bakery"
 cat_bakery_h2.style.textAlign ="center" 
 
-// appending child to cache parent element
 cat_bakery.appendChild(cat_bakery_h2);  
 
 
-// cache element using getElementById
+// for Food
 const cat_food= document.getElementById("category_food")
 
-// creating h2 tag to display below gift category
 let cat_food_h2 = document.createElement("h2")    
 cat_food_h2.innerText ="Food"
 cat_food_h2.style.textAlign ="center" 
 
-// appending child to cache parent element
 cat_food.appendChild(cat_food_h2);  
 
 
-// Event Listener :  added function to chnage theme color
+//*** Event Listener-1 :  added function to change theme color by clicking button ob banner */
 const themeColorBtn = document.getElementById("theme_color");
 
 function changeColor(){
@@ -48,36 +45,74 @@ function changeColor(){
 
     const body = document.querySelector("body")
     body.style.backgroundColor = "var(--background-color)";
+
+    themeColorBtn.style.display = 'none';
 }
 
 // added event Listener for button click 
 themeColorBtn.addEventListener('click', changeColor);
 
 
-// Event Listener :  added function to chnage theme color
+
+
+//** Event Listener-2 :  added function open popup */
 const giftImage = document.getElementById("quickLinkItemsImages");
-console.log(giftImage);
 
+let content = false;  // variable declared to avoid ultiple times content adding to popup box
+function openGiftBox(){
 
-function openBox(){
-    alert("Hi")
+    // changing display property of popup and overlay to show popup
+    document.getElementById('popup').style.display = 'block';
+    document.getElementById('overlay').style.display = 'block';
+
+    // cache element using getElementById
+    const giftListing = document.getElementById("giftsListing")
+    //console.log(content);
+
+    //added condition to check if content is already available in popup box, if not added i.e first time add below content to popup box
+    if (!content) {
+        const giftListingItms = document.createElement("h2")    
+        
+        giftListingItms.innerHTML += `<ul>`
+        giftListingItms.innerHTML += `<li><img src="../images/heart1.png" alt="Heart" height="50px" width="50px" >Heart</li>`
+        giftListingItms.innerHTML += `<li><img src="../images/trueFriends.png" alt="Heart" height="50px" width="50px" >Friends</li>`
+        giftListingItms.innerHTML += `<li><img src="../images/cups.png" alt="Heart" height="50px" width="50px" >Cups</li>`
+        giftListingItms.innerHTML +=`</ul>`
+
+        content = true; // setting var content to true to avoid adding content multiple times
+    
+        // appending child to cache parent element
+        giftListing.appendChild(giftListingItms); 
+    }
+
 }
-
 // added event Listener for button click 
-giftImage.addEventListener('click', openBox);
+giftImage.addEventListener('click', openGiftBox);
  
+// popup closed whe close button clicked
+document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+});
 
+// popup closed whe overlay clicked
+document.getElementById('overlay').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+    document.getElementById('overlay').style.display = 'none';
+});
 
-  
-  // adding more menu links
-  const ul = document.body
-  .appendChild(document.createElement("div"))
-  .appendChild(document.createElement("ul"));
-
-for (let i = 0; i < 3; i++) {
-  ol.appendChild(document.createElement("li"));
-  ul.appendChild(document.createElement("li")).innerHTML="<p class='color'> Random text</p>";
-  ul.appendChild(document.createElement("p"));
-}
-
+/** Below code added just to iterate over elements to fine image tag with id and then apply border to it */
+ 
+const containers = document.querySelectorAll('.categories_row');
+//Iterate over a collection of elements
+containers.forEach(container => {
+    // Select all image elements inside the current container
+    const images = container.querySelectorAll('img');
+    // Iterate over each image
+    images.forEach(img => {
+        if (img.hasAttribute('id')) {
+            img.classList.add('hover-border');
+        }
+    });
+});
   
